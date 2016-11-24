8028b233a20a34d6267ce059220b4e4be63173e0
linkMHran <menghaoran.link@foxmail.ocm>
2016-11-24 16:41:45 +0800

    index support

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+$(function(){
+    /*主体功能实现*/
+    (function(){
+            var oBgImg=$("#bgImg");
+            var oInfo=$(".info");
+            $(window).resize(function(){
+                oBgImg.css("height",$(window).outerHeight());
+            });
+            oBgImg.css("height",$(window).outerHeight());
+            var oBtn=oBgImg.find("a");
+            var oMain=$("#main");
+            oBtn.click(function(){
+                /*var oHeight=oBgImg.outerHeight();
+                $(window).scrollTop(oHeight);*/
+                oMain.animate({top:0},500);
+            });
+        }
+    )();
+});
