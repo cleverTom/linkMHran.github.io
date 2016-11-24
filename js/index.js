df0f47e748bbd83449f9e3bcbebf70000b77de10
linkMHran <menghaoran.link@foxmail.ocm>
2016-11-24 18:30:45 +0800

    趋于完善版

--- js/index.js
+++ js/index.js
@@ -4,19 +4,27 @@ $(function(){
     (function(){
             var oBgImg=$("#bgImg");
             var oInfo=$(".info");
+            var oReturn=$("#btn1");
+            var oBtn=oBgImg.find("a");
+            var oMain=$("#main");
             $(window).resize(function(){
                 oBgImg.css("height",$(window).outerHeight());
             });
             oBgImg.css("height",$(window).outerHeight());
-            var oBtn=oBgImg.find("a");
-            var oMain=$("#main");
             oBtn.click(function(){
                 /*var oHeight=oBgImg.outerHeight();
                 $(window).scrollTop(oHeight);*/
                 oMain.animate({top:0},500,function(){
                     oInfo.hide();
+                    oReturn.css("display","block");
                 });
                 oMain.height($(window).outerHeight());
+            });
+            oReturn.click(function(){
+                oReturn.css("display","none");
+                oMain.animate({top:$(window).outerHeight()},500,function(){
+                    oInfo.show();
+                });
             });
         }
     )();
